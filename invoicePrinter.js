84fa7de46bfc1b5563f3ae46631e245ab3f3e4d9
Jimmy DC <jimmydalecleveland@gmail.com>
2020-04-08 09:04:47 -0600

    refactor: declare function from function variable

--- invoicePrinter.js
+++ invoicePrinter.js
@@ -34,24 +34,27 @@ module.exports = function statement(invoice, plays) {
     return result
   }
 
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100)
+  }
+
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format
 
   for (let perf of invoice.performances) {
     // print line for this order
     volumeCredits += volumeCreditsFor(perf)
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`
     totalAmount += amountFor(perf)
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `Amount owed is ${usd(totalAmount)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
 }
